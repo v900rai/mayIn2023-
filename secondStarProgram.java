b7a0feb6916597810b4f18ca37d99e7af6481081
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-15 18:05:19 +0530

    Add files via upload

---
+++ secondStarProgram.java
@@ -0,0 +1,18 @@
+package javaInterviewProgram;
+
+public class secondStarProgram 
+{
+	public static  void main(String []args)
+	{
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=5; j>=i; j--)
+			{
+				System.out.print(i/2);
+				
+			}
+			System.out.println();
+		}
+	}
+
+}
