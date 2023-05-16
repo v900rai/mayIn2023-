3d2327bf21d29aa0537bc3b29af22f2734abba86
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-16 12:26:40 +0530

    Add files via upload

---
+++ ss.java
@@ -0,0 +1,17 @@
+package javaInterviewProgram;
+
+public class ss 
+{
+	public static void main(String []args)
+	{
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(i%2);
+			}
+			System.out.println();
+		}
+	}
+
+}
