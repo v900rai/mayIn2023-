74d6d9a2d14af180666191d1429f361ac79d225e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-16 12:17:51 +0530

    Add files via upload

---
+++ CharacterPattern.java
@@ -0,0 +1,17 @@
+package javaInterviewProgram;
+
+public class CharacterPattern 
+{
+	public static void main(String []args)
+	{
+		for(char i='A'; i<='E'; i++)
+		{
+			for(char j='A'; j<=i; j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+
+}
