4aeae2276e679cf810059dac49d22b40346cb591
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-17 23:51:44 +0530

    Add files via upload

---
+++ logicalpattern.java
@@ -0,0 +1,19 @@
+package selfpratices;
+
+public class logicalpattern
+{
+	public static void main(String[] args)
+	{
+		for(int i=0; i<=10; i++)
+		{
+			System.out.print(i+" ");
+		}
+		System.out.println();
+		System.out.println("======================");
+		for(int i=10; i>=0; i--)
+		{
+			System.out.print(i+" ");
+		}
+	}
+
+}
