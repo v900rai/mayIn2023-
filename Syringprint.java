04eaa68e3c88a191bff2519baf23b3fb7d8e80c7
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-19 21:54:53 +0530

    Add files via upload

---
+++ Syringprint.java
@@ -0,0 +1,13 @@
+package javaInterviewProgram;
+import java.util.*;
+public class Syringprint 
+{
+	public static void main(String []args)
+	{
+		String s1=new String ("vishalraiVishal");
+		{
+			System.out.println(s1);
+		}
+	}
+
+}
