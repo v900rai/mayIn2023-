27359e4b5d3f2dd76c2450ac46d7445e633c2a22
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-19 22:05:05 +0530

    Add files via upload

---
+++ cocatingInString.java
@@ -0,0 +1,14 @@
+package javaInterviewProgram;
+
+public class cocatingInString 
+{
+	public static void main(String []args)
+	{
+		String s="vishal";
+		s=s.concat("Rai");
+		
+		System.out.print(s);
+	}
+	
+
+}
