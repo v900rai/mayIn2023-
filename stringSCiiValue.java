98e050ad109a57b68a3d9386447e64ff264004c0
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-19 22:11:31 +0530

    Add files via upload

---
+++ stringSCiiValue.java
@@ -0,0 +1,12 @@
+package javaInterviewProgram;
+
+public class stringSCiiValue 
+{
+	public static void main(String args[]){
+		byte ascii[]={71,70,71};
+		    
+		  String s1= new String(ascii);
+		  System.out.println(s1);
+
+}
+}
