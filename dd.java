a8ccd873b8e0bc931041bb0b0ec1c7bc7c0797cf
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-20 23:36:05 +0530

    Add files via upload

---
+++ dd.java
@@ -0,0 +1,16 @@
+class  dd
+{
+	public static void main(String[] args) 
+	{
+		String s="vishal1234";
+		for(int i=0; i<=s.length()-1; i++)
+		{
+			if (s.charAt>='0' &&  s.charAt<='64')
+
+			{
+				System.out.println(s.charAt(i));
+			}
+		}
+	}
+	
+}
