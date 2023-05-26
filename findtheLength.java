88924d3d8c09c4cd6ac3c63f377a47064cc83489
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-26 15:15:44 +0530

    Add files via upload

---
+++ findtheLength.java
@@ -0,0 +1,15 @@
+package javaInterviewProgram;
+
+public class findtheLength 
+{
+	public static void main(String []args)
+	{
+		int arr[]= {10,20,30,40,50};
+		for(int i=0; i<arr.length; i++)
+		{
+			System.out.println("find the lemgth");
+			System.out.print(arr[i]+" ,");
+		}
+	}
+
+}
