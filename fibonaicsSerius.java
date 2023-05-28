9981df54b25626604b050025f16da6fa364d9954
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-28 22:13:47 +0530

    Add files via upload

---
+++ fibonaicsSerius.java
@@ -0,0 +1,38 @@
+package OctoberProgram;
+
+public class fibonaicsSerius 
+{
+	public static void main(String [] args)
+	{
+		int length=7;
+		int n1=0;
+		int n2=1;
+		if(length==0)
+		{
+			System.out.println("invailed input");
+		}
+		else if(length==1)
+		{
+			System.out.println(n1);
+		}
+		else if(length==2)
+		{
+			System.out.println(n1);
+			System.out.println(n2);
+		}
+		else
+		{
+			System.out.println(n1);
+			System.out.println(n2);
+			for(int i=3; i<=length; i++)
+			{
+				int n3=n1+n2;
+				System.out.println(n3);
+				n1=n2;
+				n2=n3;
+			}
+		}
+		
+	}
+
+}
