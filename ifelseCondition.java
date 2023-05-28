fa55cdfff49eb730a805535b697bc4fc12448bf4
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-28 22:16:33 +0530

    Add files via upload

---
+++ ifelseCondition.java
@@ -0,0 +1,25 @@
+package OctoberProgram;
+
+import java.util.Scanner;
+
+public class ifelseCondition 
+{
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the password....");
+		int pass=sc.nextInt();
+		if(pass==1212)
+		{
+			System.out.println("My name is: vishal Rai");
+			System.out.println("my age: 24");
+			System.out.println("my address : arystrdfylgu;hgfcgmj");
+		}
+		else 
+		{
+			System.out.println("Sorry! worng password....");
+		}
+		
+	}
+
+
+}
