3297fd86163a6d885f55c30a4847f34930be6911
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-28 22:14:14 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,16 @@
+package OctoberProgram;
+
+public class forloop 
+{
+	public static void main(String vishal[])
+	{
+		int count=0;
+		for(int i=1; i<=10; i++)
+		{
+		
+			count++;
+		}
+		System.out.println(count);
+	}
+
+}
