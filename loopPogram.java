512a4024467138e53e548747c42c33d7bbb83e3d
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-28 22:17:05 +0530

    Add files via upload

---
+++ loopPogram.java
@@ -0,0 +1,15 @@
+package OctoberProgram;
+
+public class loopPogram 
+{
+	public static void main(String []args)
+	{
+		int number=1;
+		while(number<=5)
+		{
+			System.out.println("vishal rai");
+			number++;
+		}
+	}
+
+}
