26d9f6b44638cc844e05b4d567540adf6b34885b
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-28 22:28:19 +0530

    Add files via upload

---
+++ ReversNumber.java
@@ -0,0 +1,22 @@
+package OctoberProgram;
+
+public class ReversNumber 
+{
+	public static void main(String [] args)
+	{
+		int count =0;
+		for(int i=100; i<=200; i++)
+		{
+			if(i%2==0)
+			{
+				
+				count++;
+				
+			}
+			System.out.println(count);
+		
+		}
+		
+	}
+
+}
