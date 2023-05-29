5be25d4f736ea2abcde6adcc14007351617e4827
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-29 23:16:05 +0530

    Add files via upload

---
+++ printThenthNumberusingforloop.java
@@ -0,0 +1,20 @@
+package edu.selfpractice.programing.string;
+
+import java.util.Scanner;
+
+public class printThenthNumberusingforloop 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value");
+		int num=sc.nextInt();
+		for(int i=0; i<=num; i++)
+		{
+			System.out.println(i);
+		}
+		
+		
+	}
+
+}
