edd78a245da008bfc80ab9dfe9ca4888a118126c
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-29 23:14:44 +0530

    Add files via upload

---
+++ Boolean.java
@@ -0,0 +1,16 @@
+package edu.selfpractice.programing.string;
+
+public class Boolean 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Boolean true and false");
+		System.out.println(10>9);
+		System.out.println(10<9);
+		System.out.println(10==10);
+		System.out.println(10!=10);
+		
+		
+	}
+
+}
