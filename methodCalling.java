a56d9d728ed360bcbf114c4ae6882193e2e4885a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-29 23:17:06 +0530

    Add files via upload

---
+++ methodCalling.java
@@ -0,0 +1,37 @@
+package edu.selfpractice.programing.string;
+
+public class methodCalling 
+{
+	public static void main(String[] args) 
+	{
+		hhhhhh(10);
+		vishal(10,  12.28);
+		System.out.println(myvishalrai(9));
+		System.out.println(raivishal(10,77));
+		int z=raiji(77,99);
+		System.out.println(z);
+		
+	}
+	public static void hhhhhh(int x)
+	{
+		System.out.println(x);
+	}
+	//System.out.println("------------------------");
+	public static void vishal(int x, double y)
+	{
+		System.out.println("x="+x+" "+y+" ");
+	}
+	public static int myvishalrai(int u)
+	{
+		return 5+u;
+	}
+	public static int raivishal(int t,int y)
+	{
+		return t+y;
+	}
+	public static int raiji(int x,int y) {
+		return x+y;
+	}
+	
+
+}
