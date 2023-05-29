d560d4712242266b5b63501e7e5784eac3bc3a76
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-29 23:14:22 +0530

    Add files via upload

---
+++ basicdemo281.java
@@ -0,0 +1,21 @@
+package edu.selfpractice.programing.string;
+
+public class basicdemo281 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("hello"+ "world");
+		System.out.println("hello"+10+20);
+		System.out.println(10+20+"hello");
+		System.out.println("hello"+'A');
+		System.out.println('A'+'B');
+		System.out.println("A"+10);
+		System.out.println('A'+10);
+		
+		
+	
+		
+	}
+	
+
+}
